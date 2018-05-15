b32df2588af7420490baa99bb37bee5159c746e6
JoaKiing <juakorabanne@gmail.com>
2018-05-15 14:15:33 -0700

    se rellena la clase data con contructor y conexion.. cambiar el nombre de la bd

--- LivingPlace/src/cl/model/Data.java
+++ LivingPlace/src/cl/model/Data.java
@@ -1,9 +1,22 @@
 package cl.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author JOAQUIN CABELLO
  */
 public class Data {
+    private ResultSet rs;
+    private String query;
+    private Conexion con;
+
+    public Data() throws SQLException, ClassNotFoundException{
+        con = new Conexion("localhost","sistema_matriculas","root","");
+    }
+    
+    
+        
     
 }
