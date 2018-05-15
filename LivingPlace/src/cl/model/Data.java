64d2d3d381403743d0bae067e931dc0e70d964ac
JoaKiing <juakorabanne@gmail.com>
2018-05-15 14:41:17 -0700

    se crea method para ver si existe rut de tabla persona

--- LivingPlace/src/cl/model/Data.java
+++ LivingPlace/src/cl/model/Data.java
@@ -16,6 +16,21 @@ public class Data {
         con = new Conexion("localhost","sistema_matriculas","root","");
     }
     
+    public boolean existeRut(int rut) throws SQLException{
+        boolean existe = false;
+        
+        query = "SELECT COUNT(*) id FROM persona WHERE run = "+rut+" ";
+        
+        rs = con.ejecutarSelect(query);
+        
+        if(rs.next()){
+            if (rs.getInt(1) == 0) {
+                existe = true;
+            }
+        }
+        
+        return existe;
+    }
     
         
     
